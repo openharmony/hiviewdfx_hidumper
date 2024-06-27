57ea359920c576dfa19d63b1718692e4f50330f0
zengxj <zengxiangjun8@h-partners.com>
2024-06-27 19:55:18 +0800

    添加单元测试用例
    Signed-off-by: zengxj <zengxiangjun8@h-partners.com>
    Change-Id: I081c3f121367818a58ce3b9acc341147527c65f1

---
+++ test/unittest/common/hidumper_client_test.cpp
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <vector>
+#include <unistd.h>
+#include <cstdlib>
+#include <gtest/gtest.h>
+#include "dump_client_main.h"
+#include "dump_controller.h"
+#include "dump_manager_client.h"
+using namespace testing::ext;
+using namespace std;
+using OHOS::HiviewDFX::DumpClientMain;
+namespace OHOS {
+namespace HiviewDFX {
+const std::string TOOL_NAME = "hidumper";
+class HidumperClientTest : public testing::Test {
+public:
+    static void SetUpTestCase(void);
+    static void TearDownTestCase(void);
+    void SetUp();
+    void TearDown();
+};
+
+void HidumperClientTest::SetUpTestCase(void)
+{
+}
+
+void HidumperClientTest::TearDownTestCase(void)
+{
+}
+
+void HidumperClientTest::SetUp(void)
+{
+}
+
+void HidumperClientTest::TearDown(void)
+{
+}
+
+/**
+ * @tc.name: ClientMainTest001
+ * @tc.desc: Test too many arguments.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HidumperClientTest, ClientMainTest001, TestSize.Level0)
+{
+    char *argv[] = {
+        const_cast<char *>(TOOL_NAME.c_str()),
+    };
+    int argc = ARG_MAX_COUNT + 1;
+    int ret = DumpClientMain::GetInstance().Main(argc, argv, STDOUT_FILENO);
+    ASSERT_EQ(ret, DumpStatus::DUMP_INVALID_ARG);
+}
+
+/**
+ * @tc.name: MemoryDumperTest002
+ * @tc.desc: Test empty argument.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HidumperClientTest, ClientTest002, TestSize.Level0)
+{
+    char *argv[] = {
+        const_cast<char *>(TOOL_NAME.c_str()),
+        const_cast<char *>("--mem"),
+        const_cast<char *>("-1"),
+        const_cast<char *>(""),
+        const_cast<char *>("--mem"),
+    };
+    int argc = sizeof(argv) / sizeof(argv[0]) - 1;
+    int ret = DumpClientMain::GetInstance().Main(argc, argv, STDOUT_FILENO);
+    ASSERT_EQ(ret, DumpStatus::DUMP_INVALID_ARG);
+}
+
+/**
+ * @tc.name: ClientMainTest003
+ * @tc.desc: Test too long argument .
+ * @tc.type: FUNC
+ */
+HWTEST_F(HidumperClientTest, ClientMainTest003, TestSize.Level0)
+{
+    std::string longArg;
+    longArg.assign(SINGLE_ARG_MAXLEN + 1, 'c');
+    char *argv[] = {
+        const_cast<char *>(TOOL_NAME.c_str()),
+        const_cast<char *>("-h"),
+        const_cast<char *>(longArg.c_str()),
+    };
+    int argc = sizeof(argv) / sizeof(argv[0]);
+    int ret = DumpClientMain::GetInstance().Main(argc, argv, STDOUT_FILENO);
+    ASSERT_EQ(ret, DumpStatus::DUMP_INVALID_ARG);
+}
+
+/**
+ * @tc.name: ClientMainTest004
+ * @tc.desc: Test null argument.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HidumperClientTest, ClientMainTest004, TestSize.Level0)
+{
+    char *argv[] = {
+        const_cast<char *>(TOOL_NAME.c_str()),
+        nullptr,
+    };
+    int argc = sizeof(argv) / sizeof(argv[0]);
+    int ret = DumpClientMain::GetInstance().Main(argc, argv, STDOUT_FILENO);
+    ASSERT_EQ(ret, DumpStatus::DUMP_INVALID_ARG);
+}
+
+/**
+ * @tc.name: ManagerClientTest001
+ * @tc.desc: Test emtpy argument list.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HidumperClientTest, ManagerClientTest001, TestSize.Level0)
+{
+    vector<u16string> args{};
+    int ret = DumpManagerClient::GetInstance().Request(args, STDOUT_FILENO);
+    ASSERT_EQ(ret, DumpStatus::DUMP_FAIL);
+}
+
+/**
+ * @tc.name: ManagerClientTest002
+ * @tc.desc: Test emtpy argument.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HidumperClientTest, ManagerClientTest002, TestSize.Level0)
+{
+    vector<u16string> args{
+        std::u16string(),
+    };
+    int ret = DumpManagerClient::GetInstance().Request(args, STDOUT_FILENO);
+    ASSERT_EQ(ret, DumpStatus::DUMP_FAIL);
+}
+}
+}
