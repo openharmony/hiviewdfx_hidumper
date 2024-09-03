8c0324bf2c18f407ff331f05be4b581bd5b1bae1
huchunlan1 <huchunlan@h-partners.com>
2024-09-03 12:08:28 +0800

    蓝牙代理网络hidumper
    Signed-off-by: huchunlan1 <huchunlan@h-partners.com>

--- frameworks/native/dump_utils.cpp
+++ frameworks/native/dump_utils.cpp
@@ -204,7 +204,7 @@ inline const std::map<int, std::string> saNameMap_ = {
     { COMM_MDNS_MANAGER_SYS_ABILITY_ID, "MDNSManager"},
     { COMM_NETSYS_EXT_SYS_ABILITY_ID, "NetsysExtService"},
     { COMM_DISTRIBUTED_NET_ABILITY_ID, "DistributedNet"},
-    { COMM_BLUETOOTH_MANAGER_SYS_ABILITY_ID, "BluetoothManager"},
+    { COMM_WATCH_DISTRIBUTED_NET_ABILITY_ID, "WatchDistributedNet"},
     { ENTERPRISE_DEVICE_MANAGER_SA_ID, "EnterpriseDeviceManagerService" },
     { I18N_SA_ID, "I18nService" },
     { PARAM_WATCHER_DISTRIBUTED_SERVICE_ID, "ParamWatcher" },
