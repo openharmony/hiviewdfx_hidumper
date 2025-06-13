f2a96b69321afd9412f62ed4ed9dffe6ae348b74
lixing <lixing141@huawei.com>
2025-06-13 12:07:51 +0800

    告警修复 Signed-off-by:lixing <lixing141@huawei.com>
    Signed-off-by: lixing <lixing141@huawei.com>

--- frameworks/native/src/executor/memory/memory_info.cpp
+++ frameworks/native/src/executor/memory/memory_info.cpp
@@ -1095,8 +1095,11 @@ bool MemoryInfo::GetMemByProcessPid(const int32_t &pid, MemInfoData::MemUsage &u
             usage.pss = usage.pss + graphicsMemory.graph;
             usage.rss = usage.rss + graphicsMemory.graph;
             DUMPER_HILOGD(MODULE_SERVICE, "uss:%{public}d pss:%{public}d rss:%{public}d gl:%{public}d graph:%{public}d",
-                static_cast<int>(usage.uss), static_cast<int>(usage.pss), static_cast<int>(usage.rss),
-                static_cast<int>(graphicsMemory.gl), static_cast<int>(graphicsMemory.graph));
+                          static_cast<int>(usage.uss),
+                          static_cast<int>(usage.pss),
+                          static_cast<int>(usage.rss),
+                          static_cast<int>(graphicsMemory.gl),
+                          static_cast<int>(graphicsMemory.graph));
         }
     }
     return success;
